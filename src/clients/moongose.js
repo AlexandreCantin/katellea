e52e0a3f16b115dcfc135e156b0796db950ae731
alexandre <cantin.alexandre@gmail.com>
2018-11-29 14:51:22 +0100

    Do not start application if mongoConnection failed

--- src/clients/moongose.js
+++ src/clients/moongose.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { environment } from '../../conf/environment';
+import * as Sentry from '@sentry/node';
 
 const databaseKey = process.env.NODE_ENV === 'testing' ? 'databaseTest' : 'database';
 const database = environment[databaseKey];
@@ -7,7 +8,13 @@ const database = environment[databaseKey];
 // MONGO_DB
 const initMongoDB = () => {
   const mongoUrl = process.env.MONGO_URL || `mongodb://${database.host}:${database.port}/${database.name}`;
-  mongoose.connect(mongoUrl, { useNewUrlParser: true });
+  mongoose.connect(mongoUrl, { useNewUrlParser: true }, err => {
+    if(err) {
+      Sentry.captureException(err);
+      console.error(err);
+      process.exit(1);
+    }
+  });
 }
 
 export default initMongoDB;
