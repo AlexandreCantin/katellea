8994af134cb056512fa2aabc36096b53bfceb285
AlexandreCantin <cantin.alexandre@gmail.com>
2019-01-17 18:41:54 +0100

    Add Katellea logo in header in tablet mode

--- frontend/src/generics/header/user/header-user.jsx
+++ frontend/src/generics/header/user/header-user.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from '@reach/router';
 
 import { MenuService } from '../../../services/menu/menu.service';
 
@@ -7,6 +8,7 @@ import HeaderAccountItem from './account-item';
 import MinimumDateItem from './minimum-date-item';
 import NotificationItem from './notification-item';
 import { extractKey } from '../../../services/helper';
+import { BetaBadge } from '../../beta-badge/beta-badge';
 
 
 require('./header-user.scss');
@@ -23,6 +25,9 @@ class HeaderUser extends Component {
     return (
       <nav className="katellea">
         <button className="hide-desktop menu-button" onClick={this.openMenu}>MENU</button>
+        <Link className="hide-desktop logo" to="/tableau-de-bord" title="Retour à l'accueil du site">
+          <img src="katellea-logo.svg" alt="K" />atellea <BetaBadge />
+        </Link>
         <HeaderAccountItem user={user} />
         <MinimumDateItem user={user} />
         <NotificationItem user={user} />
